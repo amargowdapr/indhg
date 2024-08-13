90e78fcbf3a0a98fc547d68712f75a3b93be6024
amargowdapr <amar469964@gmail.com>
2024-08-13 21:25:38 +0530

    amargowdaaggre

--- aggregation.py
+++ aggregation.py
@@ -1,21 +1,21 @@
 import numpy as np
-#Create a vector as a Row
-vector_row = np.array([ 1,2,3,4,5,6])
-# print vector
-print(vector_row)
-matrix = np.array([[1,2,3],[4,5,6],[7,8,9]])  
-#Create a Matrix 
-print(matrix)
-#Select 3rd element of Vector
-print(vector_row[2])    
-#Select 2nd row 2nd column
-print(matrix[1,1])              
-#Select all elements of a vector
-print(vector_row[:])
-#Select everything up to and including the 3rd element
-print(vector_row[:3])
-#Select the everything after the 3rd element
-print(vector_row[3:])
-#Select the last element
-print(vector_row[-1])
-#Select the first 2 rows and all the columns of the matrix
+a = np.array([1, 2, 3, 4, 5])
+print(a)
+sum_a = np.sum(a)  # Numpy function
+print("sum",sum_a)
+median_a = np.median(a)  # Numpy function
+print("median",median_a)
+mean_a = np.mean(a)  # Numpy function
+print("mean",mean_a)
+minimum_a = np.min(a)  # Numpy function
+print("minimum",minimum_a)
+maximum_a = np.max(a)  # Numpy function
+print("maximum",maximum_a)
+imin_a = np.argmin(a)  # Numpy function
+print("imin",imin_a)
+imax_a = np.argmax(a)  # Numpy function
+print("imax",imax_a)
+standard_deviation_a = np.std(a)  # Numpy function
+print("standard deviation",standard_deviation_a)
+variance_a = np.var(a)  # Numpy function
+print("variance",variance_a)
